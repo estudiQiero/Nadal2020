c811f52e16e4ac26b3fbb8c4058709fe6d93e580
Miq Alarcia <miq145@gmail.com>
2020-12-18 11:46:58 +0100

    Media queries

--- js/click.js
+++ js/click.js
@@ -7,14 +7,14 @@ llumOff.addEventListener('click', aLesFosques);
 llumOn.addEventListener('click', aLesClares);
 
 function aLesFosques() {
-    document.querySelector('.nad-cuerpo').style.background = "#000";
+    document.querySelector('body').style.background = "#000";
     document.querySelector('h1').style.color = '#000';
     document.querySelector('.nad-contSwitchImg').style.backgroundImage = 'url(img/SwitchOff.png)';
     document.querySelector('#lletresDeLlum').style.display = "block";
 }
 
 function aLesClares() {
-    document.querySelector('.nad-cuerpo').style.background = "#FFF";
+    document.querySelector('body').style.background = "#FFF";
     document.querySelector('h1').style.color = '#888';
     document.querySelector('.nad-contSwitchImg').style.backgroundImage = 'url(img/SwitchOn.png)';
     document.querySelector('#lletresDeLlum').style.display = "none";
